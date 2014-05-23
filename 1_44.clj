dd73b4e9757d8c589aa58d51d18dd7677c965e3c
Sergio Rodriguez <rodriguezvalencia@gmail.com>
2014-05-23 23:06:43 +0100

    Done with chapter 1 :)

---
+++ 1_44.clj
@@ -0,0 +1,12 @@
+(:use ["1_43.clj"])
+
+(def dx 0.000001)
+
+(defn avg [& params]
+  (/ (reduce + params) (count params)))
+
+(defn smooth [f]
+  #(avg (f (- % dx)) (f %) (f (+ % dx))))
+
+(defn n-smooth [f n]
+  (repeated smooth n) f)
---
+++ 1_46.clj
@@ -0,0 +1,15 @@
+(:use ["1_35.clj" "1_43.clj"])
+
+(defn improve-sqrt [x guess]
+  (/ (+ guess (/ x guess)) 2) )
+
+(defn good? [x nextX]
+  (< (abs (- x nextX)) 0.0001))
+
+(defn iterative-improve [good? improve guess]
+  #(if (good? guess (improve % guess))
+     guess
+     ((iterative-improve good? improve (improve % guess)) %))
+  )
+
+(float ((iterative-improve good? improve-sqrt 1) 2))
