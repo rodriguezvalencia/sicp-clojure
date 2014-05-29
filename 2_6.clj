9dfd58f19f35a69bc00d9d653ddf0188b69fdfe5
Sergio Rodriguez <rodriguezvalencia@gmail.com>
2014-05-29 22:25:26 +0100

    After almost 10 years I finally unerstand Church numerals. Wheee\!

---
+++ 2_6.clj
@@ -0,0 +1,21 @@
+(defn zero []
+  (fn [f] (fn [x] x)))
+
+(defn add-1 [n]
+  (fn [f] (fn [x] (f ((n f) x)))))
+
+(defn one []
+  (fn [f] (fn [x] (f x))))
+
+(defn two []
+  (fn [f] (fn [x] (f (f x)))))
+
+(defn three []
+  (fn [f] (fn [x] (f (f (f x))))))
+
+(defn add [a b]
+  (fn [f] (fn [x] (((a) f) (((b) f) x)) )))
+
+(defn pow [a b]
+  (fn [f] (fn [x]
+            (((b) (a)) x))))
