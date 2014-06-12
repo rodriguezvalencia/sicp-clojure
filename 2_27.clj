137064e9fa57c2a7e722ce8102068fb73a78f038
Sergio Rodriguez <rodriguezvalencia@gmail.com>
2014-06-12 22:26:09 +0100

    deep-reverse took me a good 10 minutes...

---
+++ 2_27.clj
@@ -0,0 +1,20 @@
+(:use "2_18.clj")
+
+(def x (list (list 1 2) (list 3 4)))
+
+(defn deep-reverse [l rl]
+  (defn aux-reverse [h t rl]
+    (if (empty? t)
+      (cons h rl)
+      (deep-reverse t (cons h rl))
+      ))
+  (let [[h & t] l]
+    (if (list? h)
+      (aux-reverse (deep-reverse h nil) t rl)
+      (aux-reverse h t rl)
+      )
+    ))
+
+(defn my-deep-reverse [l]
+  (deep-reverse l nil)
+  )
