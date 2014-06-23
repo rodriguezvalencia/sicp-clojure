a3e754e6e10c46077467978b46a9a346d4808efe
Sergio Rodriguez <rodriguezvalencia@gmail.com>
2014-06-23 23:22:09 +0100

    Finished queens problem

--- 2_42.clj
+++ 2_42.clj
@@ -1,11 +1,22 @@
 (load-file "2_40.clj")
+
+; each entry represents a row/column in the board, containing an integer for the queen at that cell
 (def empty-board (list))
 
+(defn safe-straights [board]
+  (= (count (set board)) (count board)))
+
+(defn safe-diag [queen board]
+  (let [diag (map #(- % queen) (range (count board)))]
+    (and (safe-straights (map + board diag))
+         (safe-straights (map + board (reverse diag))))))
+
 (defn safe? [queen board]
-  true)  ;TODO implement
+  (and (safe-straights board)
+    (safe-diag queen board)))
 
 (defn adjoin-position [new-row k rest-of-queens]
-  (list))  ;TODO implement
+  (cons new-row rest-of-queens))
 
 (defn queens [board-size]
   (defn queen-cols [k]
@@ -17,6 +28,6 @@
           (fn [rest-of-queens]
             (map (fn [new-row]
                    (adjoin-position new-row k rest-of-queens))
-              (range 1 board-size)))
+              (range board-size)))
           (queen-cols (- k 1))))))
   (queen-cols board-size))
